c8f31cd1377b3cf94a5bcf73e9f21050bd34e59a
David Cazal <da.cazal95@gmail.com>
2024-11-18 14:36:15 -0500

    Challenge completed

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,14 @@
 class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    if params[:ask] == "i am going to work now"
+      @answer = "Great!"
+    elsif params[:ask].include?("?")
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
 end
